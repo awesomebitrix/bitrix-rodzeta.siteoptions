724b44c2c8084ba197c34ebfa9df120a7c5d97d7
Rick Deckard <xxxblasteroid@gmail.com>
2016-10-27 08:41:44 +0300

    fix

--- include.php
+++ include.php
@@ -7,19 +7,16 @@
 
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
 
-use Bitrix\Main\EventManager;
+require __DIR__ . "/.init.php";
 
-define("\Rodzeta\Siteoptions\_APP", __DIR__ . "/");
-define("\Rodzeta\Siteoptions\_LIB", __DIR__  . "/lib/");
-define("\Rodzeta\Siteoptions\_FILE_OPTIONS", "/upload/.rodzeta.siteoptions.php");
-define("\Rodzeta\Siteoptions\_FILE_CSV", "/upload/.rodzeta.siteoptions.csv");
+use Bitrix\Main\EventManager;
 
 EventManager::getInstance()->addEventHandler("main", "OnBeforeProlog", function () {
 	if (CSite::InDir("/bitrix/")) {
 		return;
 	}
 	global $APPLICATION;
-	$GLOBALS["RODZETA"]["SITE"] = \Rodzeta\Siteoptions\Utils::get();
+	$GLOBALS["RODZETA"]["SITE"] = \Rodzeta\Siteoptions\Options();
 });
 
 EventManager::getInstance()->addEventHandler("main", "OnEndBufferContent", function (&$content) {
