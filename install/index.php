b4d7610fae9f73b7ecb0fbf5cce680c10fff71bf
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-19 08:33:02 +0300

    reuirement-utf8

--- install/index.php
+++ install/index.php
@@ -68,9 +68,14 @@ class rodzeta_siteoptions extends CModule {
 	}
 
 	function DoInstall() {
-		if (version_compare(PHP_VERSION, '7', '<')) {
-			global $APPLICATION;
-   		$APPLICATION->ThrowException(Loc::getMessage("RODZETA_REQUIREMENTS_PHP_VERSION"));
+		// check module requirements
+		global $APPLICATION;
+		if (version_compare(PHP_VERSION, "7", "<")) {
+			$APPLICATION->ThrowException(Loc::getMessage("RODZETA_REQUIREMENTS_PHP_VERSION"));
+			return false;
+		}
+		if (!defined("BX_UTF")) {
+			$APPLICATION->ThrowException(Loc::getMessage("RODZETA_REQUIREMENTS_BITRIX_UTF8"));
 			return false;
 		}
 
