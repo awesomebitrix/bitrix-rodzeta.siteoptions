40c1484c19a393b6861a6b6d612f0534967ea71c
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-19 11:32:04 +0300

    edit-page-example

--- install/admin/rodzeta.siteoptions/index.php
+++ install/admin/rodzeta.siteoptions/index.php
@@ -1,3 +1,42 @@
 <?php
+/*******************************************************************************
+ * rodzeta.siteoptions - Users site options
+ * Copyright 2016 Semenov Roman
+ * MIT License
+ ******************************************************************************/
 
-echo "TODO edit page";
+namespace Rodzeta\Siteoptions;
+
+require $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php";
+
+// TODO заменить на определение доступа к редактированию конента
+if (!$GLOBALS["USER"]->IsAdmin()) {
+  return;
+}
+
+$formSaved = check_bitrix_sessid() && $_SERVER["REQUEST_METHOD"] == "POST";
+
+?>
+
+<b>TODO form here</b>
+
+<?php if (!empty($formSaved)) { ?>
+
+	<script>
+		// close after submit
+		top.BX.WindowManager.Get().AllowClose();
+		top.BX.WindowManager.Get().Close();
+	</script>
+
+<?php } else { ?>
+
+	<script>
+		// add buttons for current windows
+		BX.WindowManager.Get().SetButtons([
+			BX.CDialog.prototype.btnSave,
+			BX.CDialog.prototype.btnCancel
+			//,BX.CDialog.prototype.btnClose
+		]);
+	</script>
+
+<?php } ?>
