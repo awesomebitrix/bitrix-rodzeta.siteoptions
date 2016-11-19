746460e9d685bd85f0145059b0cccd431973e17d
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-19 18:26:45 +0300

    fix

--- lang/ru/admin/rodzeta.siteoptions/index.php
+++ lang/ru/admin/rodzeta.siteoptions/index.php
@@ -1,5 +1,5 @@
 <?php
-defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+defined("B_PROLOG_INCLUDED") and (B_PROLOG_INCLUDED === true) or die();
 
 $MESS["RODZETA_SITEOPTIONS_MAIN_TAB_SET"] = "Настройки";
 $MESS["RODZETA_SITEOPTIONS_MAIN_TAB_TITLE_SET"] = "Настройка параметров модуля";
