9d712f53ac9e14c64109100a30e46ab0bf8d5d86
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-26 10:05:23 +0300

    fix

--- install/version.php
+++ install/version.php
@@ -3,6 +3,6 @@
 // NOTE this file must compatible with php 5.3
 
 $arModuleVersion = array(
-	'VERSION' 		=> '2016.11.25',
-	'VERSION_DATE' 	=> '2016-11-25'
+	'VERSION' 		=> '2016.11.26',
+	'VERSION_DATE' 	=> '2016-11-26'
 );
